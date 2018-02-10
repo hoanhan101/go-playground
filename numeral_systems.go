d56ea45c30034922461982f011b850f1c4605839
Hoanh An <hoanhan101@gmail.com>
2018-02-10 18:29:12 -0500

    fmt numerial system

--- numeral_systems.go
+++ numeral_systems.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Printf("Hexadecimal represenation of number %d is %x\n", number, number)
 	fmt.Printf("Hexadecimal represenation of number %d with leading 0 is %#x\n", number, number)
 
-    for i := 0; i < 10; i++ {
-        fmt.Printf("Decimal: %d \t Binary: %b \t Hex: %#x \t UTF-8: %q \n", i, i, i, i)
-    }
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Decimal: %d \t Binary: %b \t Hex: %#x \t UTF-8: %q \n", i, i, i, i)
+	}
 }
