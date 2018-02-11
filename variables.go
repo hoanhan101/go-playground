71afa817314b404b64da211346da454ae4b5f485
Hoanh An <hoanhan101@gmail.com>
2018-02-11 10:10:35 -0500

    add variables

---
+++ variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Shorthand for declaring and initializing variables
+    a := 1
+    b := "string"
+    c := true
+    d := 1.0
+
+    fmt.Printf("Default value is %v, which has type %T\n", a, a)
+    fmt.Printf("Default value is %v, which has type %T\n", b, b)
+    fmt.Printf("Default value is %v, which has type %T\n", c, c)
+    fmt.Printf("Default value is %v, which has type %T\n", d, d)
+}
