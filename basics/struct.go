63bab0544512541040461de29d636efb4b87a976
Hoanh An <hoanhan101@gmail.com>
2018-03-05 13:27:19 -0500

    reorganize directory

---
+++ basics/struct.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+)
+
+func main() {
+    hoanh := Person{"Hoanh", "An", 21}
+    hoanh.toString()
+}
+
+type Person struct {
+    firstName string
+    lastName string
+    age int
+}
+
+func (p Person) toString() {
+    fmt.Printf("%v %v %v", p.firstName, p.lastName, strconv.Itoa(p.age))
+}
