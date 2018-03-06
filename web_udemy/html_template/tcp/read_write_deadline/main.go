dac09efaf5d1cff584f15c576fb317a4d677a80b
Hoanh An <hoanhan101@gmail.com>
2018-03-05 23:22:12 -0500

    add read, write and timeoute

---
+++ web_udemy/html_template/tcp/read_write_deadline/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+        
+        // Use go routine to handle connection
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+    // Set a timeoute for a connection
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Fatalln("CONN TIMEOUT")
+	}
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+	}
+	defer conn.Close()
+
+	// now we get here
+	// the connection will time out
+	// that breaks us out of the scanner loop
+	fmt.Println("***CODE GOT HERE***")
+}
