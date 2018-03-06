cff1c8bd6aebf796980b249f864ef564c31d0ccc
Hoanh An <hoanhan101@gmail.com>
2018-03-05 22:36:28 -0500

    add tcp write

---
+++ web_udemy/html_template/tcp/write/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	// Listen
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		// Accept the connection
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		// Write to connection
+		io.WriteString(conn, "\nHello from TCP server\n")
+		fmt.Fprintln(conn, "How is your day?")
+		fmt.Fprintf(conn, "%v", "Well, I hope!")
+
+		conn.Close()
+	}
+}
