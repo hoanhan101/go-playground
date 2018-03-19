de4d130c68754b5ad8046a1296688ca251a8ab7f
Hoanh An <hoanhan101@gmail.com>
2018-03-19 16:54:09 -0400

    add concurrency problem

---
+++ udemy/concurrency/channel.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"time"
+    "sync"
+)
+
+func main() {
+    c_semaphore_2()
+}
+
+func c_unbuffered() {
+	// This is unbuffered channel
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			// This stops right here until until something takes the value off,
+			// which is what we are doing in the next goroutine function
+			c <- i
+		}
+	}()
+
+	go func() {
+		for {
+			fmt.Println(<-c)
+		}
+	}()
+
+	// Wait for some time for these above functions to execute. Otherwise, need
+	// to use WaitGroup
+	time.Sleep(time.Second)
+
+}
+
+func c_range() {
+    c := make(chan int)
+
+    go func() {
+        for i := 0; i <10; i ++ {
+            c <- i
+        }
+        close(c)
+    }()
+
+    for i := range c {
+        fmt.Println(i)
+    }
+}
+
+func c_waitGroup() {
+    c := make(chan int)
+
+    var wg sync.WaitGroup
+    wg.Add(1)
+
+    go func() {
+        for i := 0; i < 10; i++ {
+            c <- i
+        }
+        wg.Done()
+    }()
+
+    go func() {
+        wg.Wait()
+        close(c)
+    }()
+
+    for i := range c {
+        fmt.Println(i)
+    }
+}
+
+func c_semaphore() {
+    c := make(chan int)
+    done := make(chan bool)
+
+    go func() {
+        for i := 0; i <10; i++ {
+            c <- i
+        }
+        done <- true
+    }()
+
+    // If we don't run this with goroutine, we will have a deadlock.
+    // We will never get to the range c if we don't use goroutine.
+    // If we don't get there, it is blocking so we hang there forever.
+    go func() {
+        <-done
+        close(c)
+    }()
+
+    for i := range c {
+        fmt.Println(i)
+    }
+}
+
+func c_semaphore_2() {
+    n := 10
+    c := make(chan int)
+    done := make(chan bool)
+
+    for i := 0; i < n; i++ {
+        go func() {
+            for i := 0; i < n; i++ {
+                c <- i
+            }
+            done <- true
+        }()
+    }
+
+    go func() {
+        for i := 0; i < n; i++ {
+            <-done
+        }
+        close(c)
+    }()
+
+    for i := range c {
+        fmt.Println(i)
+    }
+}
---
+++ udemy/concurrency/wait.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	wg.Add(2)
+	go foo()
+	go bar()
+	wg.Wait()
+
+	fmt.Println("main")
+	fmt.Println("NumCPU: ", runtime.NumCPU())
+	fmt.Println("NumCgoCall: ", runtime.NumCgoCall())
+	fmt.Println("NumGoroutine: ", runtime.NumGoroutine())
+}
+
+func foo() {
+	fmt.Println("Foo")
+	fmt.Println("NumCPU: ", runtime.NumCPU())
+	fmt.Println("NumCgoCall: ", runtime.NumCgoCall())
+	fmt.Println("NumGoroutine: ", runtime.NumGoroutine())
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo: ", i)
+		time.Sleep(time.Duration(3 * time.Millisecond))
+	}
+	wg.Done()
+}
+
+func bar() {
+	fmt.Println("Bar")
+	fmt.Println("NumCPU: ", runtime.NumCPU())
+	fmt.Println("NumCgoCall: ", runtime.NumCgoCall())
+	fmt.Println("NumGoroutine: ", runtime.NumGoroutine())
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("bar: ", i)
+		time.Sleep(time.Duration(20 * time.Millisecond))
+	}
+	wg.Done()
+}
