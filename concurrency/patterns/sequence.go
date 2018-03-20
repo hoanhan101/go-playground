cc886eff21c974ef4f9d5f0b44fd5cff7e800e94
Hoanh An <hoanhan101@gmail.com>
2018-03-20 14:11:57 -0400

    restore sequence

---
+++ concurrency/patterns/sequence.go
@@ -0,0 +1,44 @@
+//
+// Sequencing (not working yet)
+//
+
+package main
+
+import (
+    "fmt"
+    "time"
+    "math/rand"
+)
+
+type Message struct {
+    str string
+    wait chan bool
+}
+
+func main() {
+    c := boring("Hoanh")
+
+    for i := 0; i < 5; i++ {
+        msg1 := <-c; fmt.Println(msg1.str)
+        msg2 := <-c; fmt.Println(msg2.str)
+        msg1.wait <- true
+        msg2.wait <- true
+    }
+
+    fmt.Println("You're boring. I am leaving!")
+}
+
+func boring(msg string) <-chan Message {
+    c := make(chan Message)
+    waitForIt := make(chan bool)
+
+    go func() {
+        for i := 0; ; i++ {
+            c <- Message{ fmt.Sprintf("%s %d", msg, i), waitForIt }
+            time.Sleep(time.Duration(rand.Intn(2e3)) * time.Millisecond)
+            <-waitForIt
+        }
+    }()
+
+    return c
+}
