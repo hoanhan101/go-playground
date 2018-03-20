9e9da09d3ee0995a1f63438c081651c50b394537
Hoanh An <hoanhan101@gmail.com>
2018-03-19 22:51:41 -0400

    add fan in fan out

---
+++ udemy/concurrency/fan_out.go
@@ -0,0 +1,66 @@
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	in := gen(2, 3)
+
+	// FAN OUT
+	// Distribute the sq work across two goroutines that both read from in.
+	c1 := sq(in)
+	c2 := sq(in)
+
+	// FAN IN
+	// Consume the merged output from multiple channels.
+	for n := range merge(c1, c2) {
+		fmt.Println(n) // 4 then 9, or 9 then 4
+	}
+}
+
+func gen(nums ...int) chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in chan int) chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func merge(cs ...chan int) chan int {
+	out := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(len(cs))
+
+	for _, c := range cs {
+		go func(ch chan int) {
+			for n := range ch {
+				out <- n
+			}
+			wg.Done()
+		}(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
