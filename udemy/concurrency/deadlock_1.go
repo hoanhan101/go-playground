ecafd1655f9e2fde3a5a797b49ad35e2ba6c2da6
Hoanh An <hoanhan101@gmail.com>
2018-03-19 20:29:39 -0400

    add deadlock challenge

---
+++ udemy/concurrency/deadlock_1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    c := make(chan int)
+
+    // Send 1 to the channel.
+    // If we do c <- 1 alone, we will create a deadlock.
+    // The solution is to wrap it within a go func().
+    go func() {
+        c <- 1
+    }()
+
+    // Read from the channel
+    fmt.Println(<-c)
+}
---
+++ udemy/concurrency/deadlock_2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    c := make(chan int)
+    go func() {
+        for i := 0; i < 10; i++ {
+            c <- i
+        }
+        close(c)
+    }()
+
+    // If we only read from <- c, then it only return 0.
+    // In order to read everything from the channel, we use range.
+    for i := range c {
+        fmt.Println(i)
+    }
+}
