2db4591d7989791a8926c28293b37d4a900cea4b
Hoanh An <hoanhan101@gmail.com>
2018-03-20 14:54:56 -0400

    add quit channel

---
+++ concurrency/patterns/quit.go
@@ -0,0 +1,41 @@
+//
+// Quit channel (not working yet)
+//
+
+package main
+
+import (
+    "fmt"
+    "time"
+    "math/rand"
+)
+
+func main() {
+    c := boring("Hoanh")
+    quit := make(chan string)
+
+    for i := rand.Intn(10); i > 0; i-- {
+        fmt.Println(<-c)
+    }
+
+    quit <- "Bye"
+    
+    fmt.Printf("%q\n",<-quit)
+}
+
+func boring(msg string) <-chan string {
+    c := make(chan string)
+
+    go func() {
+        select {
+        case s := <-c:
+            fmt.Println(s)
+        case <-quit:
+            cleanup()
+            quit <- "See you!"
+            return
+        }
+    }()
+
+    return c
+}
