89ed514f5cb76b212c546afba8c23da3ac0cc7fc
Hoanh An <hoanhan101@gmail.com>
2018-03-21 14:17:24 -0400

    add time

---
+++ concurrency/time.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    start := time.Now()
+    time.Sleep(1 * time.Second)
+
+    go func() {
+        fmt.Println(time.Now().Sub(start))
+    }()
+
+    go func() {
+        fmt.Println(time.Since(start))
+    }()
+
+    time.Sleep(1 * time.Second)
+}
